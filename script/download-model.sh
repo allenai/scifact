03c19bcc0f53d77eae072181416d283b45c1dbb7
Peter Lin <peterlin9863@gmail.com>
2020-06-01 17:53:27 -0700

    Update scripts

--- script/download-model.sh
+++ script/download-model.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+#
 # Download pre-trained model from AI2 S3 bucket.
+#
 # Usage: bash script/download-model.sh [model-component] [bert-variant] [training-dataset]
-# Example: bash script/download-model.sh rationale roberta_base fever_scifact
+# [model-component] options: "rationale", "label"
+# [bert-variant] options: "roberta_large", "roberta_base", "scibert", "biomed_roberta_base"
+# [training-dataset] options: "scifact", "fever_scifact", "fever", "snopes"
 
-# See `baselines.md` for more details on the available models.
 
 model_dir="model"
 mkdir -p "$model_dir"
 
-base="https://ai2-s2-scifact.s3-us-west-2.amazonaws.com/release/2020-05-01/models"
+base="https://scifact.s3-us-west-2.amazonaws.com/release/2020-05-01/models"
 model_component=$1
 bert_variant=$2
 training_dataset=$3
@@ -17,13 +21,13 @@ model_name="${model_component}_${bert_variant}_${training_dataset}"
 # If the file already exists, don't download again.
 if [ -e model/$model_name ]
 then
-    echo "Output directory $model_name already exists."
+    echo "Model $model_name already exists. Skip download."
     exit 0
 fi
 
-# Grab the compressed version.
+# Download the compressed tarbell.
 wget "$base/${model_name}.tar.gz" -P "$model_dir"
-# Decompress.
+# Extract the tarbell.
 tar -xzf "$model_dir/${model_name}.tar.gz" -C "$model_dir"
-# Get rid of the tarball.
+# Remove the tarball.
 rm "$model_dir/${model_name}.tar.gz"
